edaa822f32951f74b671e4f60712168180b2d25b
David Mwangi <davidmwangi@microsoft.com>
2024-09-19 10:46:36 +0300

    Add Hello World Files

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* This is just a comment*/
+int main() {
+    printf("Hello My good , People");
+    printf("Answer %d\n, 42");
+    printf("Name %s\n", "David Mwangi");
+    printf("x %.1f %d\n", 3.5, 100);
+
+    return 0;
+}
