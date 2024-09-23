7751833b5d6bee1cb5a8f90bed76ca8ce948f19d
David Mwangi <davidmwangi@microsoft.com>
2024-09-23 11:53:49 +0300

    Intro to Pointers

---
+++ managing_data_and_memory_allocation/pointers_intro.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int my_number = 10;
+
+    // Declare a pointer;
+    int *my_ptr;
+
+    // Getting the address of my_number using the "address operator - &"
+    printf("Address of 'my_number': %p\n", &my_number);
+
+    // Initialize a pointer using the "address operator - &"
+    my_ptr = &my_number;
+
+    // Initialize a pointer using  malloc
+    char *name = (char *)malloc(8 * sizeof(char));
+
+    // Dereference a ppinter to read a value at its address
+    printf("Value of 'my_number' : %d\n", *my_ptr);
+
+    int my_copied_number = *my_ptr;
+
+    printf("Value of  'my_copied_number': %d\n", my_copied_number);
+
+    // Dereference a pointer to assign a value to its address
+    *my_ptr = 20;
+
+    printf("Value of 'my_number' after assigning a new value: %d\n", *my_ptr);
+
+    free(name);
+
+    return 0;
+}
